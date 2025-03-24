db12465285e963b40d3c641c9f16a9f91d27bbbb
Chris Williamson <chriswill0629@gmail.com>
2025-03-24 12:07:58 -0400

    Testing versioning

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,33 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
+import pkg from '../../../package.json'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+    const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+    const version = pkg.version
+    return (
+        <>
+            <img alt="logo" className="logo" src={electronLogo} />
+            <div className="creator">Powered by electron-vite</div>
+            <div>Version: {version}</div>
+            <p className="tip">
+                Please try pressing <code>F12</code> to open the devTool
+            </p>
+            <div className="actions">
+                <div className="action">
+                    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+                        Documentation
+                    </a>
+                </div>
+                <div className="action">
+                    <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+                        Send IPC
+                    </a>
+                </div>
+            </div>
+            <Versions></Versions>
+        </>
+    )
 }
 
 export default App
